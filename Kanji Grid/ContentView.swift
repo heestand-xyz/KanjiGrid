a3627e9440b2da9382f51aa8c26777ac6bd25f2e
Anton Heestand <anton.heestand@gmail.com>
2022-08-14 19:29:05 +0200

    Initial Commit

---
+++ Kanji Grid/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Kanji Grid
+//
+//  Created by Anton Heestand on 2022-08-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kanji Grid/Kanji_GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kanji_GridApp.swift
+//  Kanji Grid
+//
+//  Created by Anton Heestand on 2022-08-14.
+//
+
+import SwiftUI
+
+@main
+struct Kanji_GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
